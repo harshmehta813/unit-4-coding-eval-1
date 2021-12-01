ec149f7ecaf4a70a70ab49d2da15dcbd423d5c2e
Harsh Mehta <87087018+harshmehta813@users.noreply.github.com>
2021-12-01 21:49:24 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -12,34 +12,69 @@ export default class App extends React.Component {
       filterYear: null,
       sortMethod: null
     };
-    handleType = (type) => {
-      this.setState({
-        filterType: type
-      });
-    };
-    handleYear = (year) => {
-      this.setState({
-        filterYear: year
-      });
-    };
-    handleSort = (order) => {
-      this.setState({
-        sortMethod: order
-      });
-    };
   }
+  handleType = (type) => {
+    this.setState({
+      filterType: type
+    });
+  };
+  handleYear = (year) => {
+    this.setState({
+      filterYear: year
+    });
+  };
+  handleSort = (order) => {
+    this.setState({
+      sortMethod: order
+    });
+  };
   render() {
     const { filterType, filterYear, sortMethod } = this.state;
     return (
       <div className="App">
         <div>
-          Year{" "}
+          Year
           {[2021, 2020].map((year) => (
             <button key={year} onClick={() => this.handleYear(year)}>
-              {year === 2021 ? "All" : year}
+              {year}
             </button>
           ))}
         </div>
+        <div>
+          type
+          {["SUV", "Hatchback", "Sedan"].map((type) => (
+            <button key={type} onClick={() => this.handleType(type)}>
+              {type}
+            </button>
+          ))}
+        </div>
+        {data
+          .filter(({ year, type }) => {
+            const { SUV, Hatchback, Sedan } = type;
+            let filterType = true;
+            if (type == "SUV") {
+              filterType = SUV ? true : false;
+            } else if (type == "Hatchback") {
+              filterType = Hatchback ? true : false;
+            } else if (type == "Sedan") {
+              filterType = Sedan ? true : false;
+            }
+            return year >= filterYear && filterType;
+          })
+          .sort((a, b) => {
+            if (sortMethod === null) {
+              return 0;
+            }
+            if (sortMethod === "ascending") {
+              return a.price - b.price;
+            }
+            if (sortMethod === "descending") {
+              return b.price - a.price;
+            }
+          })
+          .map((item) => (
+            <car data={item} key={item.id} />
+          ))}
       </div>
     );
   }
